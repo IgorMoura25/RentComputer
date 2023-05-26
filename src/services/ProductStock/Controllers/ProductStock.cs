ee01b2b346d9d7e7d2a55aea8b291fdc2f77fe28
Thiago Moura <thb.moura@gmail.com>
2023-05-26 15:50:41 -0300

    Feat(Estoque):Criação da API Base

---
+++ src/services/ProductStock/Controllers/ProductStock.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductStock : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello Word");
+        }
+    }
+}
